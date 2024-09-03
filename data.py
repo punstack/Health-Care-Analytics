f113826f8dfc7b71a8da67ac41575fbd291838e2
Amara Gammon <argammon413@gmail.com>
2024-09-03 09:18:45 -0400

    defined functions to upload csv files to SQL database

--- data.py
+++ data.py
@@ -19,12 +19,69 @@ database = config['mysql']['database']
 connection_string = f'mysql+mysqlconnector://{user}:{password}@{host}/{database}'
 engine = create_engine(connection_string)
 
-data = pd.read_csv("mimic-iii-clinical-database-demo-1.4/ADMISSIONS.csv")
+def update_db(csv_file_names:list):
+    '''
+        load and write data to an SQL table with the same name as the CSV file in MIMIC III
+    '''
+    for csv_file in csv_file_names:
+        if csv_file == "CHARTEVENTS":
+            chartevents_db()
+        data = pd.read_csv(f"mimic-iii-clinical-database-demo-1.4/{csv_file}.csv")
+        try:
+            data.to_sql(csv_file.lower(), con=engine, if_exists='replace', index=False)
+            print(f"{csv_file} was uploaded to SQL database successfully.")
+        except:
+            print(f"{csv_file} could not be uploaded to the database.")
 
-data.to_sql('admissions', con=engine, if_exists='replace', index=False)
+def chartevents_db():
+    dtype = {
+        'value': 'str',
+        'valueuom': 'str',
+        'resultstatus': 'str',
+        'stopped': 'str'
+    }
 
-with engine.connect() as connection:
-    query = text("SELECT row_id AS id FROM admissions LIMIT 5")
-    result = connection.execute(query)
-    for row in result:
-        print(row)
+    chunksize = 50000
+    for chunk in pd.read_csv("mimic-iii-clinical-database-demo-1.4/CHARTEVENTS.csv", dtype = dtype, chunksize = chunksize):
+        try:
+            chunk.to_sql("chartevents", con=engine, if_exists='append', index=False)
+            print("CHARTEVENTS chunk was uploaded to SQL database successfully.")
+        except:
+            print("CHARTEVENTS chunk could not be uploaded to the database.")
+            engine.dispose()
+
+if __name__ == "__main__":
+    '''
+    update_db(["ADMISSIONS",
+                "CALLOUT",
+                "CAREGIVERS",
+                "CHARTEVENTS", # this is giving errors :(
+                "CPTEVENTS",
+                "D_CPT",
+                "D_ICD_DIAGNOSES",
+                "D_ICD_PROCEDURES",
+                "D_ITEMS",
+                "D_LABITEMS",
+                "DATETIMEEVENTS",
+                "DIAGNOSES_ICD",
+                "DRGCODES",
+                "ICUSTAYS",
+                "INPUTEVENTS_CV",
+                "INPUTEVENTS_MV",
+                "LABEVENTS",
+                "MICROBIOLOGYEVENTS",
+                "NOTEEVENTS",
+                "OUTPUTEVENTS",
+                "PATIENTS",
+                "PRESCRIPTIONS",
+                "PROCEDUREEVENTS_MV",
+                "PROCEDURES_ICD",
+                "SERVICES",
+                "TRANSFERS"])
+    chartevents_db()
+    '''     
+    with engine.connect() as connection:
+        query = text("SELECT row_id  FROM chartevents LIMIT 5")
+        result = connection.execute(query)
+        for row in result:
+            print(row)
